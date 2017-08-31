9117235c849729273362dc0d33fd5d6be125e2d4
Prasann <prasann6897@gmail.com>
2017-08-30 20:16:21 -0400

    Started the UI for making the tab views

---
+++ Bazaar/ParentViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ParentViewController.swift
+//  Bazaar
+//
+//  Created by Prasann Pandya on 2017-08-30.
+//  Copyright © 2017 Syed Abbas. All rights reserved.
+//
+
+import Foundation
+//import the library
+import XLPagerTabStrip
+//Delete UIViewController, and Extend //ButtonBarPagerTabStripViewController instead
+class ParentViewController: UIViewController {
+    
+}
