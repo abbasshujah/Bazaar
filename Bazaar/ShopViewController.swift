a20aa317afcb67e5de9e12ed616acb527b779b01
Prasann <prasann6897@gmail.com>
2017-08-31 09:27:07 -0400

    Tabview working with click of search button

--- Bazaar/ShopViewController.swift
+++ Bazaar/ShopViewController.swift
@@ -7,22 +7,21 @@
 //
 
 import UIKit
-//import XLPagerTabStrip
+import XLPagerTabStrip
 
 //class ShopViewController: ButtonBarPagerTabStripViewController {
 
+//    let purpleInspireColor = UIColor(red:0.13, green:0.03, blue:0.25, alpha:1.0)
+//
 class ShopViewController: UIViewController {
-
     
+
     var adress_variable = "test"
     var shop_name = "test"
-    
-    
-    
 
     override func viewDidLoad() {
         
-        super.viewDidLoad()
+        
         
         print ("trying to print")
         print (shop_name)
@@ -31,8 +30,26 @@ class ShopViewController: UIViewController {
         //adress.text = adress_variable
         //Shop_name_outlet.text = shop_name
         
+//        TODO: Code for Tabviews
         
-        
+//        // change selected bar color
+//        settings.style.buttonBarBackgroundColor = .white
+//        settings.style.buttonBarItemBackgroundColor = .white
+//        settings.style.selectedBarBackgroundColor = purpleInspireColor
+//        settings.style.buttonBarItemFont = .boldSystemFont(ofSize: 14)
+//        settings.style.selectedBarHeight = 2.0
+//        settings.style.buttonBarMinimumLineSpacing = 0
+//        settings.style.buttonBarItemTitleColor = .black
+//        settings.style.buttonBarItemsShouldFillAvailiableWidth = true
+//        settings.style.buttonBarLeftContentInset = 0
+//        settings.style.buttonBarRightContentInset = 0
+//        changeCurrentIndexProgressive = { [weak self] (oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
+//            guard changeCurrentIndex == true else { return }
+//            oldCell?.label.textColor = .black
+//            newCell?.label.textColor = self?.purpleInspireColor
+//        
+//        }
+        super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
