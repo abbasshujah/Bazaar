f0fa1bd2aa4d8f5b5da536b5e02c23c61c85e76e
Prasann <prasann6897@gmail.com>
2017-09-03 17:39:07 -0400

    Images load up on imageview after parsing urls from firebase

--- Bazaar/ImageViewController.swift
+++ Bazaar/ImageViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2017 Syed Abbas. All rights reserved.
 //
 
+//import Foundation
 import UIKit
 import Firebase
 import Alamofire
@@ -14,14 +15,15 @@ class ImageViewController: UIViewController {
     
     static var imageCache = NSCache<AnyObject, AnyObject>()
     
+    var request: Request?
+    
     var url = ""
     
-    @IBOutlet weak var img: UIImageView!
-    
-    
+    @IBOutlet var imgView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        showImage()
     }
     
     override func didReceiveMemoryWarning() {
@@ -29,19 +31,45 @@ class ImageViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-//    func showImage() {
-////        TODO: Get Url from firebase
-//        let ref = Database.database().reference()
-//        ref.child("Basilique").child("StoreImage").observe(.value) { (snap: DataSnapshot) in
-//            let url = snap.value as! String
-//        }
-    
-//        img = ImageViewController.imageCache.object(forKey: url) as! UIImage
+    func showImage() {
+//        TODO: Get Url from firebase
         
-      
-//        ref.child("Product Info").observe(.value) { (snapshot: DataSnapshot) in
-//            let snapshotValue = snapshot.value as! String
-//            self.showProductInfo.text = snapshotValue
-//        }
-//    }
+        let ref = Database.database().reference()
+        ref.child("ShoopersDrugMart").child("StoreImage").observe(.value) { (snap: DataSnapshot) in
+            self.url = snap.value as! String
+            print(self.url)
+//            imgView.image = [UIImage ]
+            
+//            let urlString = "https://pbs.twimg.com/profile_images/780881863497752576/f1NgnOP8.jpg"
+            
+//            TODO: Show the image on imageview
+            guard let url = URL(string: self.url) else { return }
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print("Failed fetching image:", error)
+                    return
+                }
+                
+                guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                    print("Not a proper HTTPURLResponse or statusCode")
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    self.imgView.image = UIImage(data: data!)
+                }
+            }.resume()
+            
+        }
+        
+
+
+        
+        
+        
+//        img = ImageViewController.imageCache.object(forKey: url) as! UIImage
+//        var url2 = NSURL.fileURL(withPath: self.url)
+//        var data = NSData.init(contentsOf: url2)
+//        var im = UIImage(data: data)
+    }
 }
