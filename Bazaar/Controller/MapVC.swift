c7ba1c2d4e1e24ff2a833b2bb612170bb76037e5
AVIN <avin@AVINs-MacBook-Air.local>
2017-11-16 23:20:28 -0500

    map with dynamic scroll view

--- Bazaar/Controller/MapVC.swift
+++ Bazaar/Controller/MapVC.swift
@@ -11,8 +11,13 @@ import MapKit
 import CoreLocation
 
 class MapVC: UIViewController {
-
+    
+    
     @IBOutlet weak var mapView: MKMapView!
+    
+    @IBOutlet weak var pullUpView: UIView!
+    
+    @IBOutlet weak var pullUpViewHeightConstraint: NSLayoutConstraint!
     
     var locationManager = CLLocationManager()
     let authorizationStatus = CLLocationManager.authorizationStatus()
@@ -23,11 +28,46 @@ class MapVC: UIViewController {
         mapView.delegate = self
         locationManager.delegate = self
         configureLocationServices()
+        animateViewUp()
+        addSwipeDown()
+        addSwipeUp()
+    }
+    
+    func animateViewUp(){
+        pullUpViewHeightConstraint.constant = 380
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    func addSwipeUp(){
+        let swipe = UISwipeGestureRecognizer(target: self, action: #selector(animatedViewUp))
+        swipe.direction = .up
+        pullUpView.addGestureRecognizer(swipe)
+    }
+    
+    func addSwipeDown(){
+        let swipe = UISwipeGestureRecognizer(target: self, action: #selector(animatedViewDown))
+        swipe.direction = .down
+        pullUpView.addGestureRecognizer(swipe)
+    }
+    
+    @objc func animatedViewDown(){
+        pullUpViewHeightConstraint.constant = 80
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc func animatedViewUp(){
+        pullUpViewHeightConstraint.constant = 450
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
     }
 
   
     @IBAction func centerMapBtnWasPressed(_ sender: Any) {
-        
         if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
             centerMapOnUserLocation()
             mapView.showsUserLocation = true
@@ -43,9 +83,9 @@ extension MapVC: MKMapViewDelegate {
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2.0, regionRadius * 2.0)
         mapView.setRegion(coordinateRegion, animated: true)
     }
-        
-}
     
+}
+
 extension MapVC: CLLocationManagerDelegate {
     
     func configureLocationServices() {
@@ -61,7 +101,7 @@ extension MapVC: CLLocationManagerDelegate {
     }
     
 }
-    
-    
+
+
 
 
